349ccec71ef515301a0090e88a6c40d8a130889a
KUNA YOGESWARA RAO <yraokuna@gmail.com>
2026-03-30 14:28:36 +0530

    [930] [Time Beats: 33.32%] [Memory Beats: 7.02%] - LeetPush

--- Binary-Subarrays-With-Sum.java
+++ Binary-Subarrays-With-Sum.java
@@ -1,28 +1,18 @@
 1class Solution {
-2
-3    public int numSubarraysWithSum(int[] nums, int goal) {
-4        return atMost(nums, goal) - atMost(nums, goal - 1);
-5    }
-6
-7    public int atMost(int[] nums, int goal) {
-8        if(goal < 0) return 0;
-9
-10        int l = 0;
-11        int sum = 0;
-12        int count = 0;
-13
-14        for(int r = 0; r < nums.length; r++) {
-15
-16            sum += nums[r];
-17
-18            while(sum > goal) {
-19                sum -= nums[l];
-20                l++;
-21            }
-22
-23            count += r - l + 1;
-24        }
-25
-26        return count;
-27    }
-28}
+2    public int numSubarraysWithSum(int[] nums, int goal) {
+3        HashMap<Integer,Integer> hm=new HashMap<>();
+4        hm.put(0,1);
+5        int sum=0;
+6        int ans=0;
+7        for(int i=0;i<nums.length;i++)
+8        {
+9            sum+=nums[i];
+10            if(hm.containsKey(sum-goal))
+11            {
+12                ans+=hm.get(sum-goal);
+13            }
+14            hm.put(sum,hm.getOrDefault(sum,0)+1);
+15        }
+16        return ans;
+17    }
+18}
